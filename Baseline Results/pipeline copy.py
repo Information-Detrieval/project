0800d5d0fe32184b572f1b448fd8766f71c8e135
NalishJain <nalish21543@iiitd.ac.in>
2024-03-10 18:18:21 +0530

    added baseline folder

---
+++ Baseline Results/pipeline copy.py
@@ -0,0 +1,139 @@
+import os.path
+
+import os
+import pickle
+import time
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    Settings,
+    PromptTemplate,
+)
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.storage.storage_context import StorageContext
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.openai import OpenAI
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+from scrape import WebScraper
+import pandas as pd
+
+class DataPipeline():
+    def __init__(self):
+        self.pkl_dir = os.path.join(os.getcwd(), "website_data" , "pkl")
+        self.OPENAI_API_KEY = "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"
+        self.PINECONE_API_KEY = "3f684ac9-1969-465f-9cb9-670f0abf84f3"
+        os.environ["OPENAI_API_KEY"] = self.OPENAI_API_KEY
+        os.environ["PINECONE_API_KEY"] = self.PINECONE_API_KEY
+        self.PERSIST_DIR = os.path.join(os.getcwd(), "storage")
+        self.pinecone_index = self.initialize_pinecone()
+        self.websites = ""
+
+    def scrape_websites(self, websites):
+        return WebScraper(websites).scrape_websites()
+
+    def scrape_sitemap(self, sitemap):
+        return WebScraper(self.websites).scraped_sitemap(sitemap)
+
+    def initialize_pinecone(self):
+        pc = Pinecone(api_key=self.PINECONE_API_KEY)
+        try:
+            index_name = "nalish"
+
+            if index_name not in pc.list_indexes().names():
+                pc.create_index(
+                    name=index_name,
+                    dimension=1536,
+                    metric="cosine",
+                    spec=ServerlessSpec(cloud="gcp-starter", region="Iowa (us-central1)"),
+                )
+
+            return pc.Index(index_name)
+
+        except Exception as e:
+            print(f"Pine Cone initialization failed: {e}")
+
+
+    def initialize_index(self, documents, vector_store):
+        embed_model = OpenAIEmbedding(model="text-embedding-ada-002", embed_batch_size=200)
+        Settings.embed_model = embed_model
+        pipeline = IngestionPipeline(
+            transformations=[
+                SentenceSplitter(
+                    chunk_overlap=200,
+                    chunk_size=1024,
+                ),
+                embed_model,
+            ],
+            vector_store=vector_store,
+        )
+        if os.path.exists(self.PERSIST_DIR):
+            storage_context = StorageContext.from_defaults(
+                persist_dir=self.PERSIST_DIR, vector_store=vector_store
+            )
+            index = load_index_from_storage(storage_context)
+            return index
+        else:
+            pipeline.run(documents=documents)
+            index = VectorStoreIndex.from_vector_store(vector_store)
+            index.storage_context.persist(persist_dir=self.PERSIST_DIR)
+            return index
+
+    def run_query(self, query_str):
+        llm = OpenAI(model="gpt-3.5-turbo-0125", api_key=self.OPENAI_API_KEY)
+        vector_store = PineconeVectorStore(pinecone_index=self.pinecone_index)
+        if os.path.exists(os.path.join(os.getcwd(), "website_data", "pkl", "documents.pkl")):
+            with open(os.path.join(os.getcwd(), "website_data", "pkl", "documents.pkl"), "rb") as f:
+                documents = pickle.load(f)
+        else:
+            documents = SimpleDirectoryReader(
+                os.path.join(os.getcwd(), "website_data", "txt"),
+                recursive=True,
+            ).load_data()
+            with open(os.path.join(os.getcwd(), "website_data", "pkl", "documents.pkl"), "wb") as f:
+                documents = pickle.dump(documents, f)
+
+        index = self.initialize_index(documents, vector_store)
+        retriever = VectorIndexRetriever(index, similarity_top_k=3)
+        retrieved_nodes = retriever.retrieve(query_str)
+        return retrieved_nodes
+
+
+
+if __name__ == "__main__":
+    websites = ["https://www.iiitd.ac.in/dhruv"]
+    df = pd.read_csv("QnA-Website2.csv")
+    temp = DataPipeline()
+    # temp.scrape_sitemap("sitemap_taj.xml")
+    new_rows = []
+
+    for index, row in df.iterrows():
+        print(index)
+        ground_truth_doc = row['Text File']  
+        query = row['Question']  
+        query_answer = row['Answer']
+        retreived_docs = temp.run_query(query)
+        r_doc1 = retreived_docs[0].metadata['file_path'][57:]
+        r_doc2 = retreived_docs[1].metadata['file_path'][57:]
+        r_doc3 = retreived_docs[2].metadata['file_path'][57:]
+
+        new_row = [query, ground_truth_doc, r_doc1, r_doc2, r_doc3] 
+        new_rows.append(new_row)
+        # break
+
+    new_df = pd.DataFrame(new_rows, columns=['Question', 'Text_File', 'Retrieved_document_1', 'Retrieved_document_2', 'Retrieved_document_3'])
+    new_df.to_csv("QnR-Taj.csv", index=False)
+
+
+
+
+
+# pinecone_index = self.initialize_pinecone()
+# vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+# documents = self.load_or_cache_documents("./cached_documents.pkl")
+# documents = self.load_or_cache_documents("./cached_documents.pkl")
+# documents = self.load_or_cache_documents("./cached_documents.pkl")
+# documents = self.load_or_cache_documents("./cached_documents.pkl")
---
+++ Baseline Results/scrape copy.py
@@ -0,0 +1,127 @@
+import re
+
+import requests
+import pickle
+from bs4 import BeautifulSoup
+import os
+
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    Settings,
+    PromptTemplate,
+)
+import xml.etree.ElementTree as ET
+
+
+class WebScraper:
+    def __init__(self, websites):
+        self.websites = websites
+        self.html_dir = os.path.join(os.getcwd(), "website_data", "html")
+        self.pkl_dir = os.path.join(os.getcwd(), "website_data", "txt")
+        self.mapping_file = "mapping.pkl"
+
+        os.makedirs(self.html_dir, exist_ok=True)
+        os.makedirs(self.pkl_dir, exist_ok=True)
+
+    def _get_filename(self, website):
+        return website.split("://")[1].replace("/", "_")
+
+    def _write_to_file(self, filepath, content):
+        with open(filepath,  "w", encoding="utf-8") as f:
+            f.write(content)
+
+    def scrape_websites(self):
+        mapping = {}
+        for website in self.websites:
+            print(f"Scraping {website}...")
+            try:
+                r = requests.get(website)
+                soup = BeautifulSoup(r.text, 'html.parser')
+                data = soup.get_text()
+
+                filename = self._get_filename(website)
+                html_filepath = os.path.join(self.html_dir, f"{filename}.html")
+                pkl_filepath = os.path.join(self.pkl_dir, f"{filename}.txt")
+
+                # self._write_to_file(pkl_filepath+".txt", data)
+                # remove repeated blank lines, but retain single new lines
+                data = re.sub('\n{2,}', '\n', data)
+
+                self._write_to_file(html_filepath, r.text)
+                self._write_to_file(pkl_filepath, data)
+
+                mapping[website] = f"{filename}.pkl"
+
+            except Exception as e:
+                print(f"Error scraping {website}: {e}")
+
+        # with open(self.mapping_file, "wb") as f:
+        #     pickle.dump(mapping, f)
+
+        return self.html_dir, self.pkl_dir
+
+
+    def get_html(self, website):
+        filename = self._get_filename(website)
+        filepath = os.path.join(self.html_dir, f"{filename}.html")
+        try:
+            with open(filepath, "r") as f:
+                html_content = f.read()
+                print(f"HTML for {website}:\n{html_content}")
+        except FileNotFoundError:
+            print(f"HTML file for {website} not found.")
+
+    def get_data(self, website):
+        filename = self._get_filename(website)
+        filepath = os.path.join(self.pkl_dir, f"{filename}.pkl")
+        try:
+            with open(filepath, "rb") as f:
+                data = pickle.load(f)
+                print(f"Data for {website}:\n{data}")
+        except FileNotFoundError:
+            print(f"Data file for {website} not found.")
+
+    def get_mapping(self):
+        try:
+            with open(self.mapping_file, "rb") as f:
+                mapping = pickle.load(f)
+                print("Mapping:")
+                for website, filename in mapping.items():
+                    print(f"{website} -> {filename}")
+        except FileNotFoundError:
+            print("Mapping file not found.")
+
+    # extract data from XML sitemap and call scrape
+
+    def scraped_sitemap(self, sitemap_file):
+        try:
+            with open(sitemap_file, "r", encoding="utf-8") as f:
+                sitemap_xml = f.read()
+                root = ET.fromstring(sitemap_xml)
+
+                # Define the namespace
+                ns = {"ns": "http://www.sitemaps.org/schemas/sitemap/0.9"}
+
+                # Use the namespace in the XPath expression
+                urls = [url.text for url in root.findall(".//ns:loc", namespaces=ns)]
+
+                self.websites = urls
+                self.scrape_websites()
+
+        except FileNotFoundError:
+            print(f"Sitemap file not found: {sitemap_file}")
+
+
+
+
+if __name__ == "__main__":
+    websites = ["https://www.iiitd.ac.in/dhruv", "https://www.iiitd.ac.in/about", "https://iiitd.ac.in/facilities/green_policy"]
+    scraper = WebScraper(websites)
+    # scraper.scrape_websites()
+    scraper.scraped_sitemap("sitemap.xml")
+
+    # with open("website_data/pkl/iiitd_ac_in_dhruv.pkl", "rb") as f:
+    #     data = pickle.load(f)
+    #     print(data)
